df4ec248f2ca7168c5453e09d6efc11030678dac
Toderean R. <raultoderean20@gmail.com>
2024-11-07 13:40:36 +0100

    i delete preloader - change home page

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-// function showSidebar() {
-//   const sidebar = document.querySelector(".sidebar");
-//   sidebar.style.display = "flex";
-// }
-document.addEventListener("DOMContentLoaded", function () {
-  // Ensure the content is hidden initially
-  document.body.classList.add("loading");
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Ensure the content is hidden initially
+//   document.body.classList.add("loading");
 
-  window.addEventListener("load", function () {
-    // Add a delay before removing the preloader
-    setTimeout(function () {
-      // Fade out the preloader
-      var preloader = document.getElementById("preloader");
-      preloader.classList.add("hidden");
+//   window.addEventListener("load", function () {
+//     // Add a delay before removing the preloader
+//     setTimeout(function () {
+//       // Fade out the preloader
+//       var preloader = document.getElementById("preloader");
+//       preloader.classList.add("hidden");
 
-      // Show the content with a fade-in effect
-      document.body.classList.remove("loading");
-      document.body.classList.add("loaded");
-      setTimeout(function () {
-        document.getElementById("content").classList.add("show");
-      }, 50); // Short delay to ensure smooth transition
-    }, 1000); // 3000 milliseconds = 3 seconds delay
-  });
-});
+//       // Show the content with a fade-in effect
+//       document.body.classList.remove("loading");
+//       document.body.classList.add("loaded");
+//       setTimeout(function () {
+//         document.getElementById("content").classList.add("show");
+//       }, 50); // Short delay to ensure smooth transition
+//     }, 1000); // 3000 milliseconds = 3 seconds delay
+//   });
+// });
 
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
